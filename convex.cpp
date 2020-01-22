1463f6bf221a7026ce6ea327927b44b4ff8fc303
mdaffad <muhammaddaffadinaya@gmail.com>
2020-01-22 07:14:22 +0700

    final

--- convex.cpp
+++ convex.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <stdlib.h>
+#include <time.h>  
 struct point
 {
     float x,y;
@@ -6,51 +8,115 @@ struct point
 struct line
 {
     point a,b;
-    bool over = false,below = false;
-};
 
+};
+bool over = false,below = false;
 int num;
 int main()
 {
     
-    FILE *fp;
+    srand (time(NULL));
     float a,b;
     int indexer = 0;
-    fp = fopen("test.txt", "r");
-    fscanf(fp,"%d",&num);
+    scanf("%d",&num);
     point input[num];
     line output[num];
     for (int i = 0; i < num; i++)
     {
-        fscanf(fp,"%f %f", &input[i].x,&input[i].y);
-        printf("%f %f\n",input[i].x, input[i].y);
+        input[i].x = rand()%100;
+        input[i].y = rand()%100;
+        // printf("%f %f\n",input[i].x, input[i].y);
     }
-    fclose(fp);
-    
+    clock_t begin = clock();
+
+
     for (int i = 0; i < num-1; i++)
     {
         for (int j = i+1; j < num; j++)
         {
             // printf("i j %d %d\n",i ,j);
+            float test;
             for (int k = 0; k < num; k++)
             {
                 // printf("i j k %d %d %d\n",i ,j, k);
-                if ((input[k].y - input[i].y) / (input[j].y - input[i].y) - (input[k].x - input[i].x) / (input[j].x - input[i].x) > 0 && i != k && j != k);
+                if (!( ((input[i].x == input[k].x) && (input[i].y == input[k].y)) || ( (input[j].x == input[k].x) && (input[j].y == input[k].y))))
                 {
-                    checker = false;
-                    // printf("bureiki %d %d\n",i ,j);
-                    break;
-                }   
+                    test = (input[j].y - input[i].y)*input[k].x + (input[i].x - input[j].x)*input[k].y - input[i].x*input[j].y + input[i].y*input[j].x; 
+                    if (test > 0)
+                    {
+                        over = true;
+                    }
+                    else if (test < 0)
+                    {
+                        below = true;
+                    }
+                    else
+                    {
+                        if  (!(   ((input[i].x < input[k].x) && (input[i].y < input[k].y) &&
+                                (input[j].x > input[k].x) && (input[j].y > input[k].y)) ||
+                                ((input[i].x < input[k].x) && (input[i].y > input[k].y) &&
+                                (input[j].x > input[k].x) && (input[j].y < input[k].y)) ||
+                                ((input[i].x > input[k].x) && (input[i].y > input[k].y) &&
+                                (input[j].x < input[k].x) && (input[j].y < input[k].y)) ||
+                                ((input[i].x > input[k].x) && (input[i].y < input[k].y) &&
+                                (input[j].x < input[k].x) && (input[j].y > input[k].y)) ||
+                                ((input[i].x < input[k].x) && (input[i].y == input[k].y) &&
+                                (input[j].x > input[k].x) && (input[j].y == input[k].y)) ||
+                                ((input[j].x < input[k].x) && (input[j].y == input[k].y) &&
+                                (input[i].x > input[k].x) && (input[i].y == input[k].y)) ||
+                                ((input[i].x == input[k].x) && (input[i].y > input[k].y) &&
+                                (input[j].x == input[k].x) && (input[j].y < input[k].y)) ||
+                                ((input[j].x == input[k].x) && (input[j].y > input[k].y) &&
+                                (input[i].x == input[k].x) && (input[i].y < input[k].y)) 
+                            ))
+                        {
+                            over = true;
+                            below = true;
+                            break;
+                        }
+                    }
+                }
+                
             }
-            if (checker)
+            if (!(below && over))
             {
                 output[indexer].a = input[i];
                 output[indexer].b = input[j];
+                // printf("true\n");
+                // printf("%f %f\n", input[i].x, input[i].y);
+                // printf("%f %f\n", input[j].x, input[j].y);
                 indexer++;
-                // printf("the truth %d %d\n",i,j);
             }
+            over = false;
+            below = false;
         }
     }
-    printf("%d \n",indexer);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+    for (int i = 0; i < indexer; i++)
+    {
+        if (i == 0 && indexer != 1)
+        {
+            printf("[(%f,%f),(%f,%f),", output[i].a.x, output[i].a.y, output[i].b.x, output[i].b.y);
+        }
+        else if((i > 0) && (i != indexer-1))
+        {
+            printf("(%f,%f),", output[i].b.x, output[i].b.y);
+        }
+        else if (indexer == 1)
+        {
+             printf("[(%f,%f),(%f,%f)]\n", output[i].a.x, output[i].a.y, output[i].b.x, output[i].b.y);
+        }
+           
+        else
+        {
+            printf("(%f,%f)]\n", output[0].a.x, output[0].a.y);
+        }
+        
+        
+    }
+    printf("%f", time_spent);
     return 0;
 }
